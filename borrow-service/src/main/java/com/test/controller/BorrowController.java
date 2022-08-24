5ee5c67c60ffdc4253ff0cda7d54308ac24db971
bySongKoy <783305523@qq.com>
2022-08-24 08:19:55 +0800

    加入读我吗可当文件

--- borrow-service/src/main/java/com/test/controller/BorrowController.java
+++ borrow-service/src/main/java/com/test/controller/BorrowController.java
@@ -1,5 +1,6 @@
 package com.test.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.test.entity.Borrow;
 import com.test.entity.UserBorrowDetail;
 import com.test.service.BorrowService;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 
 @RestController
 public class BorrowController {
@@ -16,9 +18,15 @@ public class BorrowController {
     @Resource
     BorrowService service;
 
+    @HystrixCommand(fallbackMethod = "onError")
     @RequestMapping("/borrow/{uid}")
      UserBorrowDetail findUserBorrows(@PathVariable("uid") int uid){
         return service.getUserBorrowDetailByUid(uid);
+    }
+
+    //备选返回方案
+    UserBorrowDetail onError(int uid){
+      return new UserBorrowDetail(null, Collections.emptyList());
     }
 
 }
